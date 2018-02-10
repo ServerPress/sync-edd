12c3f04f1aad255eef9005bd99e8a01290777b05
Dave Jesch <dave@davejesch.com>
2018-02-09 18:53:18 -0800

    check for items contained in bundle #7; update URLs in download meta data #10

--- classes/eddapirequest.php
+++ classes/eddapirequest.php
@@ -11,6 +11,7 @@ class SyncEDDApiRequest
 	const ERROR_EDD_NOT_ON_TARGET = 1003;
 	const ERROR_BAD_EDD_VERSION = 1004;
 	const ERROR_MINIMUM_VERSION = 1005;
+	const ERROR_MISSING_PRODUCT_FROM_BUNDLE = 1006;
 
 	const NOTICE = 1000;
 
@@ -25,13 +26,13 @@ class SyncEDDApiRequest
 	public function error_code_to_string($message, $code)
 	{
 		switch ($code) {
-		case self::ERROR_VERSION_NOT_SUPPORTED:		$message = __('This version of Easy Digital Downloads is not supported. Please upgrade to 3.0 or above.', 'wpsitesync-edd');		break;
-		case self::ERROR_EDD_VERSION_MISMATCH:		$message = __('The version of Easy Digital Downloads on Source and Target sites must match. Please upgrade.', 'wpsitesync-edd');	break;
-		case self::ERROR_BUNDLE_PRODUCT_NOT_SYNCD:	$message = __('One of the Products in your Bundle has not yet been Pushed to the Target site.', 'wpsitesync-edd');	break;
-		case self::ERROR_EDD_NOT_ON_TARGET:			$message = __('Push failed. WPSiteSync for EDD is not active on Target.', 'wpsitesync-edd'); break;
-		case self::ERROR_BAD_EDD_VERSION:			$message = __('The version of Easy Digital Downloads on the Target site cannot be detected.', 'wpsitesync-edd'); break;
-		case self::ERROR_MINIMUM_VERSION:			$message = sprintf(__('The version of Easy Digital Downloads on the Target needs to be at least %1$d.', 'wpsitesync-edd'), self::MIN_EDD_VERSION); break;
-//		case self::ERROR_EDD_VERSION_MISMATCH:		$message = __('The EDD versions are different on the Source and Target sites.', 'wpsitesync-edd'); break;
+		case self::ERROR_VERSION_NOT_SUPPORTED:			$message = __('This version of Easy Digital Downloads is not supported. Please upgrade to 3.0 or above.', 'wpsitesync-edd');		break;
+		case self::ERROR_EDD_VERSION_MISMATCH:			$message = __('The version of Easy Digital Downloads on Source and Target sites must match. Please upgrade.', 'wpsitesync-edd');	break;
+		case self::ERROR_BUNDLE_PRODUCT_NOT_SYNCD:		$message = __('One of the Products in your Bundle has not yet been Pushed to the Target site.', 'wpsitesync-edd');	break;
+		case self::ERROR_EDD_NOT_ON_TARGET:				$message = __('Push failed. WPSiteSync for EDD is not active on Target.', 'wpsitesync-edd'); break;
+		case self::ERROR_BAD_EDD_VERSION:				$message = __('The version of Easy Digital Downloads on the Target site cannot be detected.', 'wpsitesync-edd'); break;
+		case self::ERROR_MINIMUM_VERSION:				$message = sprintf(__('The version of Easy Digital Downloads on the Target needs to be at least %1$d.', 'wpsitesync-edd'), self::MIN_EDD_VERSION); break;
+		case self::ERROR_MISSING_PRODUCT_FROM_BUNDLE:	$message = __('One of the Products in the Bundle has not yet been Syncd to the Target site.', 'wpsitesync-edd'); break;
 		}
 		return $message;
 	}
